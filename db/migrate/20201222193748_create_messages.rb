5f42f09bffd610487e1cb20e67d37b7c5823548b
shunxile <69545945+shunxile@users.noreply.github.com>
2020-12-23 05:51:46 +0900

    新規作成＆ER図

---
+++ db/migrate/20201222193748_create_messages.rb
@@ -0,0 +1,11 @@
+class CreateMessages < ActiveRecord::Migration[6.0]
+  def change
+    create_table :messages do |t|
+      t.string :title, null: false
+      t.text :message
+      t.data :open_plan
+      t.string :encrypted_password
+      t.timestamps
+    end
+  end
+end
