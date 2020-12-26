6f65c07705ac8421d2c8ecfb9de5b949c78bc791
shunxile <69545945+shunxile@users.noreply.github.com>
2020-12-27 03:59:00 +0900

    新規登録ページ

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @messages = Message.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :messages, only: :index
+  resources :user
   root to: "messages#index"
 end
