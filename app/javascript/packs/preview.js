843f14ea1e8685d7dca89085499b332d619dc587
shunxile <69545945+shunxile@users.noreply.github.com>
2021-01-11 15:44:59 +0900

    プレビュー機能 JavaScript条件付け

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,16 +1,18 @@
-document.addEventListener('DOMContentLoaded', function(){
-  const ImageList = document.getElementById('image-list');
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+  document.addEventListener('DOMContentLoaded', function(){
+    const ImageList = document.getElementById('image-list');
 
-  document.getElementById('form-image').addEventListener('change', function(e){
-    const file = e.target.files[0];
-    const blob = window.URL.createObjectURL(file);
+    document.getElementById('form-image').addEventListener('change', function(e){
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
 
-    const imageElement = document.createElement('div');
+      const imageElement = document.createElement('div');
 
-    const blobImage = document.createElement('img');
-    blobImage.setAttribute('src', blob);
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('src', blob);
 
-    imageElement.appendChild(blobImage);
-    ImageList.appendChild(imageElement);
+      imageElement.appendChild(blobImage);
+      ImageList.appendChild(imageElement);
+    });
   });
-});
+}
